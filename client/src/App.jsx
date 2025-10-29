b706809c7afeef6230bc3819ba7bdce8ed0e28a7
ChetanVarunKanipakam <chetanvarun131@gmail.com>
2025-10-29 19:46:48 +0530

    added providers to main app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,47 @@ import Navbar from './components/Navbar.jsx';
 import HomePage from './pages/HomePage.jsx';
 import SubjectPage from './pages/SubjectPage.jsx';
 import LessonPage from './pages/LessonPage.jsx';
-
-
+import SubjectLessonPage from './pages/SubjectLessonPage.jsx';
+import SubjectList from './components/SubjectList.jsx';
+import LessonList from './components/LessonList.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import Dashboard from './pages/DashboardPage.jsx';
+import {ThemeProvider }from '@mui/material/styles';
+// Import new pages
+import DiscussionsPage from './pages/DiscussionsPage.jsx';
+import DiscussionDetailsPage from './pages/DiscussionDetailsPage.jsx';
+import NewDiscussionPage from './pages/NewDiscussionPage.jsx';
+import theme from './theme.js';
+import Layout from './components/Layout.jsx';
+import MyDiscussionsPage from './pages/MyDiscussionPage.jsx';
 function App() {
   return (
+
+     <AuthProvider>
     <Router>
-      <CssBaseline />
-      <Box sx={{ backgroundColor: '#F8F9FA', minHeight: '100vh' }}>
-        <Navbar />
+      <CssBaseline /> 
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/subjects/:subjectId/lessons" element={<SubjectLessonPage />} />
+            <Route path="/subjects/:subjectId/lessons/:lessonId" element={<SubjectLessonPage />} />
             <Route path="/subjects/:subjectId" element={<SubjectPage />} />
             <Route path="/lessons/:lessonId" element={<LessonPage />} />
+            <Route path="/subjectlist" element={<SubjectList/>} />
+            <Route path="/subjects/:subjectId/lessonslist" element={<LessonList/>} />
+            <Route path="/alldiscussions" element={<DiscussionsPage />} />
+            {/* Add new discussion routes here */}
+            <Route element={<Layout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/discussions" element={<DiscussionsPage />} />
+              <Route path="/my-discussions" element={<MyDiscussionsPage />} />
+              <Route path="/discussions/:id" element={<DiscussionDetailsPage />} />
+              <Route path="/new-discussion" element={<NewDiscussionPage />} />
+           </Route>
           </Routes>
         </main>
-      </Box>
     </Router>
+    </AuthProvider>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId="373322380870-ub2ud7q0ofd2aporuhsqeqee2964oegt.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
